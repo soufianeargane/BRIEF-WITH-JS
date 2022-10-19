7eb91a5cdb1695c7f2842b5db398076d6715ca60
soufianeargane <112890451+soufianeargane@users.noreply.github.com>
2022-10-19 20:56:01 +0100

    working on update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,23 +4,25 @@
  */
 
 
- var index;
- var myTitle ;
- var myFeature = document.getElementById("feature");
- var myBug = document.getElementById("bug");
- var myType;
+var index;
+var myTitle ;
+var myFeature = document.getElementById("feature");
+var myBug = document.getElementById("bug");
+var myType;
+var myPriority;
+var myStatus;
 
 
- var high = document.getElementById("high");
- var medium = document.getElementById("medium");
- var low = document.getElementById("low");
- var critical = document.getElementById("critical");
- var myPriority;
+ var priority = document.getElementById("formSelectPriority");
+ var arr_Priority = Array.from(priority)
+
+  var statuss =  document.getElementById("formSelectStatus");
+  var arr_Status = Array.from(statuss);
 
  var myToDo = document.getElementById("To Do");
  var myDoing = document.getElementById("In Progress");
  var myDone = document.getElementById("Done");
- var myStatus
+
  var myDate
  var myDescription
 
@@ -31,19 +33,21 @@ function createTask() {
     // Ouvrir modal form
     
     myTitle = document.getElementById("formTitle").value;
-    console.log(myTitle);
-
     if(myFeature.checked) {myType= document.getElementById("feature").id}
     if(myBug.checked){myType= document.getElementById("bug").id;}
 
-    if(high.selected){myStatus = document.getElementById("high").id}
-    if(medium.selected){myPriority = document.getElementById("medium").id}
-    if(low.selected){myPriority = document.getElementById("low").id}
-    if(critical.selected){myPriority = document.getElementById("critical").id}
-
-    if(myToDo.selected){myStatus = document.getElementById("To Do").id}
-    if(myDoing.selected){myStatus = document.getElementById("In Progress").id}
-    if(myDone.selected){myStatus = document.getElementById("Done").id}
+    arr_Priority.forEach(function(option){
+        if(option.selected){
+            myPriority = option.value
+        }
+        })
+        // console.log(myPriority);
+    
+    arr_Status.forEach(function(option){
+        if(option.selected){
+            myStatus = option.value
+        }
+    })
 
     myDate=document.getElementById("endDate").value
     myDescription = document.getElementById("formTextarea").value
@@ -56,7 +60,6 @@ function createTask() {
         date         : myDate,
         description  : myDescription,
     })
-    console.log(tasks);
     readTask()
 }
 
@@ -70,9 +73,11 @@ function readTask(){
     myDos.innerHTML= '';
     myDoings.innerHTML='';
     myDones.innerHTML='';
+    
     for(let i = 0; i < tasks.length; i++){
+        index = i;
         if( tasks[i].status === "To Do" ){
-            myDos.innerHTML +=`<button type="button" data-bs-toggle="modal" data-bs-target="#modal-task-edit" class="w-100 border-0 mb-1 bg-white d-flex" onclick="editTask(${i})"  >
+            myDos.innerHTML +=`<button id="edit-btn" type="button" data-bs-toggle="modal" data-bs-target="#modal-task-edit" class="w-100 border-0 mb-1 bg-white d-flex" onclick="editTask(${index})" >
          					<div class="p-2">
          						<i class="bi bi-question-circle text-green-500 fs-4"></i> 
          					</div>
@@ -80,7 +85,7 @@ function readTask(){
          						<div class="fw-bolder h5 mb-1 ">  ${tasks[i].title}</div>
          						<div class="d-flex flex-column text-start">
          							<div class="text-gray-600 mb-1">#1 created in  ${tasks[i].date}</div>
-         							<div class="mb-2" title=""> ${tasks[i].description}</div>
+         							<div class="mb-2 text-truncate" style="max-width: 16rem;" title="${tasks[i].description}"> ${tasks[i].description}</div>
          						</div>
          						<div class="">
         							<span class="rounded px-2 py-1 text-white bg-cyan-600"> ${tasks[i].priority}</span>
@@ -90,7 +95,7 @@ function readTask(){
          				</button> `
         }
         if(tasks[i].status === "In Progress"){
-            myDoings.innerHTML +=`<button type="button" data-bs-toggle="modal" data-bs-target="#modal-task-edit" class="w-100 border-0 mb-1 bg-white d-flex" onclick="editTask()" >
+            myDoings.innerHTML +=`<button id="edit-btn" type="button" data-bs-toggle="modal" data-bs-target="#modal-task-edit" class="w-100 border-0 mb-1 bg-white d-flex" onclick="editTask(${index})">
                             <div class="p-2">
                                 <i class="bi bi-question-circle text-green-500 fs-4"></i> 
                             </div>
@@ -98,7 +103,7 @@ function readTask(){
                                 <div class="fw-bolder h5 mb-1 ">  ${tasks[i].title}</div>
                                 <div class="d-flex flex-column text-start">
                                     <div class="text-gray-600 mb-1">#1 created in  ${tasks[i].date}</div>
-                                    <div class="mb-2" title=""> ${tasks[i].description}</div>
+                                    <div class="mb-2 text-truncate" style="max-width: 16rem;" title="${tasks[i].description}"> ${tasks[i].description}</div>
                                 </div>
                                 <div class="">
                                 <span class="rounded px-2 py-1 text-white bg-cyan-600"> ${tasks[i].priority}</span>
@@ -108,7 +113,7 @@ function readTask(){
                         </button> `
         }
         if(tasks[i].status === "Done" ){
-            myDones.innerHTML +=`<button  type="button" data-bs-toggle="modal" data-bs-target="#modal-task-edit" class="w-100 border-0 mb-1 bg-white d-flex" onclick="editTask()" >
+            myDones.innerHTML +=`<button id="edit-btn" type="button" data-bs-toggle="modal" data-bs-target="#modal-task-edit" class="w-100 border-0 mb-1 bg-white d-flex" onclick="editTask(${index})">
                                 <div class="p-2">
                                     <i class="bi bi-question-circle text-green-500 fs-4"></i> 
                                 </div>
@@ -116,7 +121,7 @@ function readTask(){
                                     <div class="fw-bolder h5 mb-1 ">  ${tasks[i].title}</div>
                                     <div class="d-flex flex-column text-start">
                                         <div class="text-gray-600 mb-1">#1 created in  ${tasks[i].date}</div>
-                                        <div class="mb-2" title="">${tasks[i].description} </div>
+                                        <div class="mb-2 text-truncate" style="max-width: 16rem;" title="${tasks[i].description}">${tasks[i].description} </div>
                                     </div>
                                     <div class="">
                                     <span class="rounded px-2 py-1 text-white bg-cyan-600"> ${tasks[i].priority}</span>
@@ -128,59 +133,40 @@ function readTask(){
     }
 }
 
-function saveTask() {
-    // Recuperer task attributes a partir les champs input
 
-    // Créez task object
+function editTask(index){
 
-    // Ajoutez object au Array
-
-    // refresh tasks
-    
-}
-
-function editTask(index) {
-    // console.log(index);
-    // Initialisez task form
-    // Affichez updates
-    // Delete Button
-    // Définir l’index en entrée cachée pour l’utiliser en Update et Delete
-    // Definir FORM INPUTS
-    // Ouvrir Modal form
-
-    let edit;
-    edit = document.getElementById("edit");
-    edit.innerHTML = `<div class="modal fade" id="modal-task-edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <!-- Modal content goes here -->
+    let place = document.getElementById("modal-task-edit");
+    place.innerHTML = `
     <div class="modal-dialog">
         <form  class="modal-content">
-            <div class="modal-header">
+          <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Add task</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-            <div   class="mb-3">
+          </div>
+          <div class="modal-body">
+          <div   class="mb-3">
               <label  class="form-label">Title</label>
-              <input type="text" class="form-control" id="formTitle">
+              <input type="text" class="form-control" id="formTitle-edit">
               <label  class="form-label mt-10px">Type</label>
               <div class="form-check ms-3">
-                  <input class="form-check-input" type="radio" name="flexRadioDefault" id="feature" >
+                  <input class="form-check-input" type="radio" name="flexRadioDefault" id="feature-edit" >
                   <label class="form-check-label" >Feature</label>
               </div>
               <div class="form-check ms-3">
-                  <input class="form-check-input" type="radio" name="flexRadioDefault" id="bug">
+                  <input class="form-check-input" type="radio" name="flexRadioDefault" id="bug-edit">
                   <label class="form-check-label" >Bug</label>
               </div>
               <label class="form-label mt-1">Priority</label>
-              <select class="form-select form-select-lg mb-3" id="formSelectPriority" aria-label=".form-select-lg example">
-                  <option disabled selected>Please select</option>
-                  <option id="high" value="1">high</option>
+              <select class="form-select form-select-lg mb-3" id="formSelectPriority-edit" aria-label=".form-select-lg example">
+                  <option  disabled selected>Please select</option>
+                  <option id="high-edit" value="1">High</option>
                   <option id="medium" value="2">medium</option>
                   <option id="low" value="3">low</option>
                   <option id="critical" value="3">critical</option>
               </select>
               <label  class="form-label mt-1">Status</label>
-              <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" id="formSelectStatus">
+              <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" id="formSelectStatus-edit">
                   <option disabled selected>Please select</option>
                   <option id="To Do" value="1">to do</option>
                   <option id="In Progress" value="2">doing</option>
@@ -188,37 +174,55 @@ function editTask(index) {
               </select>
               <label  class="form-label mt-1">Date</label>
               <div>
-                  <input type="date" id="endDate" name="trip-start"
+                  <input type="date" id="endDate-edit" name="trip-start"
                   value="yyyy-MM-dd" class="form-control mb-3"
                   min="2021-01-01" max="2023-12-31">
               </div>
               <div class="mb-3">
                   <label class="form-label">Description</label>
-                  <textarea class="form-control" id="formTextarea" rows="3"></textarea>
+                  <textarea class="form-control" id="formTextarea-edit" rows="3"></textarea>
               </div>
               <div class="modal-footer">
-                  <button type="button"  class="btn btn-light text-dark" data-bs-dismiss="modal">Close</button>
-                  <button type="button" id="update"  class="btn btn-success" data-bs-dismiss="modal">Update</button>
-                  <button type="button" id="delete"  class="btn btn btn-danger" data-bs-dismiss="modal">Delete</button> 
-              </div>
-          </div>
-      </div>
-  </form>
-  </div>
-</div>`
+                <button type="button"  class="btn btn-light text-dark" data-bs-dismiss="modal">Close</button>
+                <button type="button" id="update" onclick="getIndex(index)" class="btn btn-success" data-bs-dismiss="modal">Update</button>
+                <button type="button" id="delete"  class="btn btn btn-danger" data-bs-dismiss="modal">Delete</button>
+                </div>
+            </div>
+        </div>
+    </form>
+    </div>`
+    document.getElementById("formTitle-edit").value = (tasks[index].title);
+    if(tasks[index].type === "Bug"){document.getElementById("bug-edit").checked = true};
+    if(tasks[index].type === "Feature"){document.getElementById("feature-edit").checked = true};
+    document.getElementById("formSelectPriority-edit").value = (tasks[index].priority);
+    document.getElementById("formSelectStatus-edit").value = tasks[index].status;
+    document.getElementById("endDate-edit").value = tasks[index].date;
+    document.getElementById("formTextarea-edit").value = tasks[index].description;
+    console.log(index);
+
 }
 
-function updateTask() {
-    // GET TASK ATTRIBUTES FROM INPUTS
 
-    // Créez task object
+var myTitle_edit ;
+// var myFeature_edit = document.getElementById("feature-edit");
+// var myBug_edit = document.getElementById("bug-edit");
+// var myType_edit;
+// var myPriority_edit;
+// var myStatus_edit;
 
-    // Remplacer ancienne task par nouvelle task
+// var priority_edit = document.getElementById("formSelectPriority-edit");
+// var arr_Priority_edit = Array.from(priority_edit)
 
-    // Fermer Modal form
+// var statuss_edit =  document.getElementById("formSelectStatus-edit");
+// var arr_Status_edit = Array.from(statuss_edit);
 
-    // Refresh tasks
-    
+
+function updateTask(index) {
+    myTitle_edit = document.getElementById("formTitle-edit").value;
+    tasks[index].title = myTitle_edit;
+    console.log(tasks[index].type);
+
+    readTask();
 }
 
 function deleteTask() {
@@ -237,13 +241,21 @@ function initTaskForm() {
     // Hide all action buttons
 }
 
-function reloadTasks() {
-    // Remove tasks elements
-
-    // Set Task count
+function getIndex(index) {
+    updateTask(index);
 }
 
 
+function saveTask() {
+    // Recuperer task attributes a partir les champs input
+
+    // Créez task object
+
+    // Ajoutez object au Array
+
+    // refresh tasks
+    
+}
 
 
     
